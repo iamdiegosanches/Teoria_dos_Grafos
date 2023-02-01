7c4987d323b598c97658a17a7ed3b13c20c24c41
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-02-01 11:00:29 -0300

    Update main.py

--- mazeSolver/main.py
+++ mazeSolver/main.py
@@ -1,44 +1,68 @@
 import pandas as pd
-import time
-from graph import Graph
+import timeit
+from Graph import Graph
+import os
 
-with open("mazes/maze3.txt") as test:
-    maze = []
-    for line in test:
-        maze.append([i for i in line.strip("\n")])
+def create_graph(arq):
+    start_time = timeit.default_timer()
+
+    with open(arq) as test:
+        maze = []
+        for line in test:
+            maze.append([i for i in line.strip("\n")])
+        df = pd.DataFrame(maze)
+
+    g1 = Graph(len(df.axes[0]), len(df.axes[1]))
+
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == "#":
+                g1.adj_list.pop((i, j))
+            else:
+                if maze[i][j] == "S":
+                    start_lab = (i, j)
+                if maze[i][j] == "E":
+                    end_lab = (i, j)
+                if i != 0:
+                    if maze[i - 1][j] != "#":
+                        g1.add_directed_edge((i, j), (i - 1, j))
+                if j != len(maze[i]) - 1:
+                    if maze[i][j + 1] != "#":
+                        g1.add_directed_edge((i, j), (i, j + 1))
+                if i != len(maze) - 1:
+                    if maze[i + 1][j] != "#":
+                        g1.add_directed_edge((i, j), (i + 1, j))
+                if j != 0:
+                    if maze[i][j - 1] != "#":
+                        g1.add_directed_edge((i, j), (i, j - 1))
+
+    end_time = timeit.default_timer()
+    execution_time = (end_time - start_time)
+
+    return df, g1, start_lab, end_lab, execution_time
+
+
+def show_walk(g1, start, end):
+    for i in g1.depth_search(start, end):
+        maze[i[1]][i[0]] = '.'
+    print(pd.DataFrame(maze))
+
     df = pd.DataFrame(maze)
-    print(df)
-    test.close()
-
-g1 = Graph(len(df.axes[0]), len(df.axes[1]))
-
-start: tuple = (0, 0)
-end: tuple = (0, 0)
-
-for i in range(len(maze)):
-    for j in range(len(maze[i])):
-        if maze[i][j] == "#":
-            g1.adj_list.pop((i, j))
-        else:
-            if maze[i][j] == "E":
-                end = (i, j)
-            if maze[i][j] == "S":
-                start = (i, j)
-            if i != 0:
-                if maze[i - 1][j] != "#":
-                    g1.add_directed_edge((i, j), (i - 1, j))
-            if j != len(maze[i]) - 1:
-                if maze[i][j + 1] != "#":
-                    g1.add_directed_edge((i, j), (i, j + 1))
-            if i != len(maze) - 1:
-                if maze[i + 1][j] != "#":
-                    g1.add_directed_edge((i, j), (i + 1, j))
-            if j != 0:
-                if maze[i][j - 1] != "#":
-                    g1.add_directed_edge((i, j), (i, j-1))
+    with open("result.txt", 'w') as f:
+        dfAsString = df.to_string(header=False, index=False)
+        f.write(dfAsString)
 
 
-# print(g1.adj_list)
-start_time = time.time()
-print(g1.depth_search(start, end))
-print("--- %s seconds ---" % (time.time() - start_time))
+op = '1'
+while op != '0':
+    op = str(input("Informe o nome do arquivo (0 para sair) "))
+    if op != '0':
+        maze, g1, start, end, execution_time = create_graph(op)
+        print("|| IMPRIMINDO LABBIRINTO ||")
+        print(maze)
+        print(f"\n|| Caminho: {g1.depth_search(start, end)}")
+        print(f"|| Tempo: {execution_time}")
+
+        show_walk(g1, start, end)
+        os.system("PAUSE")
+        os.system('cls')
